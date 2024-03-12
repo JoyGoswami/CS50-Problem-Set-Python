86c047ce3728b081f4ebc0158275d143e0fceb42
Joy Goswami <joy63586@gmail.com>
2024-03-12 20:26:07 +0600

    Tip Calculator added

---
+++ Problem Set 0/Tip Calculator/tip.py
@@ -0,0 +1,20 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    remove_sign = d.lstrip('$')
+    convert_to_float = float(remove_sign)
+    return convert_to_float
+    # TODO
+
+
+def percent_to_float(p):
+    return float(p.rstrip("%")) / 100
+    # TODO
+
+
+main()
